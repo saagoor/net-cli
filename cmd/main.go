af8001c3c8e3562eec9a71312ba83ca054523326
MH Sagor <sagor@gotipath.com>
2022-07-20 00:31:14 +0600

    Nameservers lookup

---
+++ cmd/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Website Lookup CLI"
+	app.Usage = "Lets you query IP's, CNAME's, MX Records & Nameservers!"
+
+	myFlags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "tutorialedge.net",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ns",
+			Usage: "Looks up the nameservers of a particular host.",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				ns, err := net.LookupNS(c.String("host"))
+				if err != nil {
+					return err
+				}
+				for i := 0; i < len(ns); i++ {
+					fmt.Println(ns[i].Host)
+				}
+				return nil
+			},
+		},
+	}
+
+	err := app.Run((os.Args))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
